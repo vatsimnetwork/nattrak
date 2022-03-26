033cc241739ddc5d40659dd40ec7f651e81cd43b
Liesel Downes <l_downes@icloud.com>
2022-03-26 18:04:43 +1030

    Maintenance view

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <style>
+        * {
+            font-family: 'Segoe UI', 'ui-sans-serif', 'sans-serif';
+            font-weight: 400;
+            text-align: center;
+        }
+
+        body {
+            background-color: #2B3990;
+        }
+
+        h2 {
+            margin-top: 20%;
+            color: #fff;
+        }
+    </style>
+</head>
+<body>
+    <h2>natTrak is currently offline. Revert to voice operations on the VATSIM network.</h2>
+</body>
+</html>
