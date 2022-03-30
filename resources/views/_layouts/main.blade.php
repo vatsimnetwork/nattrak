8bd81b1fd3b9dc2b870efddccf0391d7ad99c7eb
Liesel Downes <l_downes@icloud.com>
2022-03-31 08:16:30 +1030

    Fix: activity log menu url

--- resources/views/_layouts/main.blade.php
+++ resources/views/_layouts/main.blade.php
@@ -62,7 +62,7 @@
                                         <a class="dropdown-item" href="{{ route('administration.accounts') }}">Admin Users</a>
                                     @endif
                                     <a href="{{ route('administration.controllers') }}" class="dropdown-item">Controller Permissions</a>
-                                    <a class="dropdown-item" href="">Activity Log</a>
+                                    <a class="dropdown-item" href="{{ route('administration.activity-log') }}">Activity Log</a>
                                 </div>
                             </li>
                             @endcan
