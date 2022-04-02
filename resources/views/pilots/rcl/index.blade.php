c366c7d352729b726d090e2fab33befa96ebae84
Liesel Downes <l_downes@icloud.com>
2022-04-02 20:50:35 +1030

    Fix: RCL timing messages

--- resources/views/pilots/rcl/index.blade.php
+++ resources/views/pilots/rcl/index.blade.php
@@ -5,7 +5,7 @@
             <p class="header">
                 Request Oceanic Clearance
             </p><hr />
-            <p>You must request oceanic clearance at least <b>30 minutes before oceanic entry.</b></p>
+            <p>You must request oceanic clearance at least <b>30 minutes before oceanic entry</b> and <b>not 2 hours beforehand (not on the ground!).</b></p>
             <p>Need help? Check out the documentation in the <i>How To Use</i> dropdown menu above.</p>
             <p><b>Please note that if you are flying across the oceanic without a CTP slot, you will be delayed and likely asked to move outside of the vertical limits of oceanic airspace.</b></p>
             <a class="btn btn-primary" href="{{ route('pilots.rcl.create') }}">Request Clearance</a>
