69e1dda0b5a2d302f5b603db9135632dba458584
Liesel Downes <lieselwd@gmail.com>
2023-02-12 19:32:56 +0800

    Add UIKit and FontAwesome 6

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
-import './bootstrap';
+import 'uikit/dist/css/uikit.min.css';
+import 'uikit/dist/js/uikit.min.js';
+import '@fortawesome/fontawesome-free/js/brands.js';
+import '@fortawesome/fontawesome-free/js/solid.js';
+import '@fortawesome/fontawesome-free/js/fontawesome';
