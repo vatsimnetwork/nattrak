afad1be89eb0cc0e16cf7daa44164349356ca7de
Liesel Downes <l_downes@icloud.com>
2023-04-28 17:02:24 +0100

    Tracks page

--- resources/views/_layouts/main.blade.php
+++ resources/views/_layouts/main.blade.php
@@ -37,7 +37,7 @@
                                             <a href="{{ route('administration.activity-log') }}" class="dropdown-item">Activity log</a>
                                         </li>
                                         <li>
-                                            <a href="#" class="dropdown-item">Tracks</a>
+                                            <a href="{{ route('tracks.index') }}" class="dropdown-item">Tracks</a>
                                         </li>
                                         <li>
                                             <a href="{{ route('notams.index') }}" class="dropdown-item">NOTAMs</a>
@@ -68,9 +68,9 @@
                             @endif
                         @endauth
                         <li class="nav-item">
-                            <div href="" class="btn btn-outline-secondary font-display" style="cursor: pointer;">
+                            <a href="{{ route('tracks.index') }}" class="btn btn-outline-secondary font-display" style="cursor: pointer;">
                                 TMI {{ current_tmi() }}
-                            </div>
+                            </a>
                         </li>
                     </ul>
                 </div>
