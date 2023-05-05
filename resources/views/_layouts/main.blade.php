67c7673af9056a38d930811f21fe3dc955975245
Liesel Downes <l_downes@icloud.com>
2023-05-05 16:34:17 +0100

    Readd links on main

--- resources/views/_layouts/main.blade.php
+++ resources/views/_layouts/main.blade.php
@@ -95,6 +95,8 @@
             {{--                <p class="col-md-4 mb-0 text-center text-body-secondary">Version x</p>--}}
 
             <ul class="nav col-md-4 justify-content-end">
+                <li class="nav-item"><a href="{{ route('tracks.index') }}"
+                                        class="nav-link px-2 text-body-secondary">Tracks</a></li>
                 <li class="nav-item"><a href="{{ route('about') }}" class="nav-link px-2 text-body-secondary">About</a></li>
                 <li class="nav-item"><a href="https://github.com/vatsimnetwork/nattrak" class="nav-link px-2 text-body-secondary">GitHub</a></li>
                 {{--                    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Report Issue</a></li>--}}
