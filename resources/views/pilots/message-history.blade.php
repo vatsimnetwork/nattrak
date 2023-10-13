42844143399239be17446f387ffdfa5a5ce8aedf
Liesel D <liesel.downes@icloud.com>
2023-10-13 18:56:58 +1030

    Adjustments

--- resources/views/pilots/message-history.blade.php
+++ resources/views/pilots/message-history.blade.php
@@ -4,11 +4,4 @@
         <h1 class="fs-2 mb-4 font-display text-primary-emphasis">Message history</h1>
         <livewire:pilots.message-history/>
     </div>
-{{--    <script type="module">--}}
-{{--        let id = {{ auth()->id() }}--}}
-{{--        Echo.private(`clearance.${id}`)--}}
-{{--            .listen('.clx.issued', (e) => {--}}
-{{--                console.log(e)--}}
-{{--            })--}}
-{{--    </script>--}}
 @endsection
--- resources/views/pilots/notify-new-eta.blade.php
+++ resources/views/pilots/notify-new-eta.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
         <h1 class="fs-2 mb-4 font-display text-primary-emphasis">Notify New Entry Time</h1>
         <p>
-            Some sort of text about when to use this form
+            You must notify the controller when your estimated entry time for oceanic airspace changes by 5 or more minutes.
         </p>
         <livewire:pilots.notify-new-eta/>
     </div>
